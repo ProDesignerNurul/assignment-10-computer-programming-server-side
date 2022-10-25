44f28c36439dc8afda787bf03f7221b8017f3ab1
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-25 09:35:52 +0600

    added categories data

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('server is running now')
+})
+
+app.listen(port, () => {
+    console.log('kaj kortese')
+})
